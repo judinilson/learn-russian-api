a2053fafdae7bb480640a4d8bd6e7ea999450415
judinilson <judinilson0000@gmail.com>
2020-07-12 20:13:10 +0400

    new actions (getbyId,put,delete)

---
+++ Models/TeacherGroup/Create/TeacherGroupModify.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_Russian_API.Models.TeacherGroup.Create
+{
+    public class TeacherGroupModify
+    {
+        public long Id { get; set; }
+        public long TeacherId { get; set; }
+         public long GroupId { get; set; }
+         public TimeSpan teaching_time { get; set; }
+    }
+}
